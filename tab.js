034d9d9c956723b8d3ada77a7d2674fd6f09dd0d
Kevin Fu <zkevinfu@gmail.com>
2018-06-04 21:23:24 -0500

    wall paper rotates

--- tab.js
+++ tab.js
@@ -1,18 +1,63 @@
-function httpGetAsync(theUrl, callback){
+function httpGetAsync(theUrl, callback, to_set) {
     var xmlHttp = new XMLHttpRequest();
-
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+    xmlHttp.onreadystatechange = function(){
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            callback(xmlHttp.responseText, to_set);
+        }
     };
     xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    //xmlHttp.setRequestHeader('User-agent', 'chrome:imgaginarylandscapes_wallpaper:v0.1 (by /u/begelsyah)');
+    //xmlHttp.setRequestHeader('User-agent', 'chrome:imaginarylandscapes_wallpaper:v0.1 (by /u/begelsyah)');
     xmlHttp.send(null);
 }
 
-function setBody(response){
+function parseData(response, to_set) {
   var resp_json = JSON.parse(response);
-  document.body.style.backgroundImage = "url("+resp_json.data.children[2].data.url+")";
+  var il_posts = resp_json.data.children;
+  var url_list = [];
+  il_posts.forEach(function(item, index) {
+    var item_url = item.data.preview;
+    if(item_url!= undefined) {
+      url_list.push(item_url.images[0].source.url);
+    }
+  });
+  if (to_set) {
+    setBackground(url_list);
+  } else {
+    saveUrlList(url_list);
+  }
 }
 
-httpGetAsync('https://www.reddit.com/r/ImaginaryLandscapes.json', setBody);
+function setBackground(url_list) {
+  //resp_json.data.children[0].data.preview.images[0].source.url
+  //resp_json.data.children[0].data.url
+  var img_url;
+  do {
+    img_url = url_list.shift();
+  } while (img_url == undefined);
+  document.body.style.backgroundImage = "url("+img_url+")";
+  if (url_list.length === 0) {
+    httpGetAsync('https://www.reddit.com/r/ImaginaryLandscapes.json', parseData, false);
+  } else {
+    saveUrlList(url_list);
+  }
+}
+
+function saveUrlList(url_list) {
+  chrome.storage.local.set({url_list: url_list});
+}
+
+function loadBackground() {
+  /*
+  syntax for after first page searches
+  https://www.reddit.com/r/imaginarylandscapes/.json?count=50&after=t3_10omtd/
+  */
+  chrome.storage.local.get(['url_list'], function(result) {
+    if (result.url_list == undefined) {
+      httpGetAsync('https://www.reddit.com/r/ImaginaryLandscapes.json', parseData, true);
+    } else {
+      setBackground(result.url_list);
+    }
+  });
+}
+
+loadBackground();
